7f9fa19041fb2c1e918f40e49c327a2948fb27bc
Dashia <das-bobkova@yandex.ru>
2017-04-19 11:34:13 +0300

    Task two done

---
+++ src/main/java/TaskTwo/Main.java
@@ -0,0 +1,27 @@
+package TaskTwo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Daria Serebryakova on 18.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context1 = new ClassPathXmlApplicationContext("taskTwoConfig.xml");
+        UniversityGroup group1 = (UniversityGroup)context1.getBean("collectionInjectionSI");
+
+        System.out.println("<!--Setter Injection-->");
+        System.out.println("group1.getStudentsList() = " + group1.getStudentsList());
+        System.out.println("group1.getStudentSet() = " + group1.getStudentSet());
+        System.out.println("group1.getStudentMap() = " + group1.getStudentMap());
+
+        ApplicationContext context2 = new ClassPathXmlApplicationContext("taskTwoConfig.xml");
+        UniversityGroup group2 = (UniversityGroup)context2.getBean("collectionInjectionCI");
+
+        System.out.println("<!--Constructor Injection-->");
+        System.out.println("group2.getStudentsList() = " + group2.getStudentsList());
+        System.out.println("group2.getStudentSet() = " + group2.getStudentSet());
+        System.out.println("group2.getStudentMap() = " + group2.getStudentMap());
+}
+}
---
+++ src/main/java/TaskTwo/UniversityGroup.java
@@ -0,0 +1,51 @@
+package TaskTwo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Daria Serebryakova on 19.04.2017.
+ */
+public class UniversityGroup {
+    private List<String> studentsList;
+    private Set<String> studentSet;
+    private Map<Integer, String> studentMap;
+
+    public UniversityGroup() {
+    }
+
+    public UniversityGroup(List<String> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public UniversityGroup(List<String> studentsList, Set<String> studentSet, Map<Integer, String> studentMap) {
+        this.studentsList = studentsList;
+        this.studentSet = studentSet;
+        this.studentMap = studentMap;
+    }
+
+    public List<String> getStudentsList() {
+        return studentsList;
+    }
+
+    public void setStudentsList(List<String> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public Set<String> getStudentSet() {
+        return studentSet;
+    }
+
+    public void setStudentSet(Set<String> studentSet) {
+        this.studentSet = studentSet;
+    }
+
+    public Map<Integer, String> getStudentMap() {
+        return studentMap;
+    }
+
+    public void setStudentMap(Map<Integer, String> studentMap) {
+        this.studentMap = studentMap;
+    }
+}
