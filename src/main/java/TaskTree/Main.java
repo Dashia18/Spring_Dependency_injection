391a7b6512450a228b2fcc4369ead12d818ded25
Dashia <das-bobkova@yandex.ru>
2017-04-19 11:55:27 +0300

    Task tree done

---
+++ src/main/java/TaskTree/Main.java
@@ -0,0 +1,19 @@
+package TaskTree;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Daria Serebryakova on 19.04.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("taskTreeConfig.xml");
+        Person person = (Person)context.getBean("person");
+        System.out.println("person = " + person);
+
+        Student student = (Student)context.getBean("student");
+        System.out.println("student = " + student);
+
+    }
+}
---
+++ src/main/java/TaskTree/Student.java
@@ -0,0 +1,54 @@
+package TaskTree;
+
+/**
+ * Created by Daria Serebryakova on 19.04.2017.
+ */
+public class Student {
+    private String name;
+    private int age;
+    private int course;
+    private int group;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", course=" + course +
+                ", group=" + group +
+                '}';
+    }
+}
